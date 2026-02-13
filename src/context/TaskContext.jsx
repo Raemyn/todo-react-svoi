57d417288bd417f791533edaf70b8dde0b4a0872
Raemyn <Zhekalah@gmail.com>
2026-02-13 15:00:40 +0500

    Формы

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,65 +1,24 @@
-import { createContext, useCallback, useEffect, useMemo, useState } from "react";
+import { createContext,  } from "react";
+import useTasks from "../hooks/useTasks";
 
 export const TaskContext = createContext({})
 export const TasksProvider = (props) => {
-    const [tasks, setTasks] = useState(() => {
-        
-        const savedTasks = localStorage.getItem('tasks')
-        if (savedTasks) return JSON.parse(savedTasks)
-        return [{ id: 'task-1', title: 'Купить молоко', isDone: false, },
-        { id: 'task-2', title: 'Погладить кота', isDone: true, },
-        ];
-
-    })
-    useEffect(() => {
-
-        localStorage.setItem('tasks', JSON.stringify(tasks))
-    }, [tasks])
-    const [newTaskTitle, setNewTaskTitle] = useState('')
-
-    const deleteALLTask = useCallback(() => {
-        const isConfirm = window.confirm('Вы уверены что хотите удалить все задачи?')
-        if (isConfirm) setTasks([])
-
-    }, [])
-
-    const deleteTask = useCallback((taskId => {
-        setTasks(
-            tasks.filter((task) => task.id !== taskId)
-        )
-    }), [tasks])
-    const toggleTaskComplete = useCallback((taskId, isDone) => {
-        setTasks(
-            tasks.map((task) => {
-                if (task.id === taskId) return { ...task, isDone }
-                return task
-            })
-        )
-    }, [tasks])
-    const addTask = useCallback(() => {
-        if (newTaskTitle.trim().length > 0) {
-            const newTask = {
-                id: crypto?.randomUUID() ?? Date.now().toString(),
-                title: newTaskTitle,
-                isDone: false,
-            }
-
-            setTasks(prev => [...prev, newTask])
-            setNewTaskTitle('')
-        }
-    }, [newTaskTitle])
-
-    const [searchQuery, setSearchQuery] = useState('');
-
-
-
-    const filtredTasks = useMemo(() => {
-        const clearSearchQuery = searchQuery.trim().toLowerCase()
-        return clearSearchQuery.length > 0
-            ? tasks.filter(({ title }) => title.toLowerCase().includes(clearSearchQuery)) : null
-    }, [searchQuery, tasks])
-
     const { children } = props
+    
+    const {
+        tasks,
+        filtredTasks,
+        deleteTask,
+        deleteALLTask,
+        toggleTaskComplete,
+        addTask,
+        newTaskTitle,
+        setNewTaskTitle,
+        searchQuery,
+        setSearchQuery,
+    } = useTasks()
+
+
     return (<TaskContext.Provider
         value={{
             tasks,
